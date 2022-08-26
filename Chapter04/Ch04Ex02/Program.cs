19130be9f36e48354de054f1b26fc54cf0c222a4
bscode2021 <bscode@hotmail.com>
2022-08-26 19:29:36 +0200

    Inital commit added chapter02, chapter03, chapter04

---
+++ Chapter04/Ch04Ex02/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Ch04Ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string comparison;
+            Console.WriteLine("Enter a number:");
+
+            double var1 = default, var2 = default;
+
+            try
+            {
+                var1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter another number:");
+                var2 = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.GetType()} says {ex.Message} \r\n {ex.StackTrace}");
+            }
+
+            if (var1 < var2)
+            {
+                comparison = "less than";
+            }
+            else
+            {
+                if (var1 == var2)
+                {
+                    comparison = "equal to";
+                }
+                else
+                {
+                    comparison = "greater than";
+                }
+
+            }
+            Console.WriteLine($"The first number is {comparison} the second number.");
+        }
+    }
+}
