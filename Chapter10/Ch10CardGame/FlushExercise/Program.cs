21b52c1ac5e1ac68f7bbe20f48bf8d1ca7656e13
bscode2021 <bscode@hotmail.com>
2022-10-04 19:03:31 +0200

    Finished Flush Exercise

---
+++ Chapter10/Ch10CardGame/FlushExercise/Program.cs
@@ -0,0 +1,51 @@
+﻿using Ch10CardLib;
+
+Deck deck = new Deck();
+
+for (int j = 0; j < 50; j++)
+{
+
+
+    Card[] hand = new Card[5];
+    Random random = new Random();
+
+    for (int i = 0; i < hand.Length; i++)
+    {
+        bool notFoundCard = true;
+        while (notFoundCard)
+        {
+            int randomIndex = random.Next(52);
+            Card card = deck.GetCard(randomIndex);
+            if (!hand.Contains(card))
+            {
+                notFoundCard = false;
+                hand[i] = card;
+            }
+        }
+    }
+
+    Suit suit = hand[0].suit;
+    bool flush = true;
+    for (int i = 1; i < hand.Length; i++)
+    {
+        if (suit != hand[i].suit)
+        {
+            flush = false;
+            break;
+        }
+    }
+
+    if (flush)
+    {
+        for (int i = 0; i < hand.Length; i++)
+        {
+            Console.WriteLine(hand[i].ToString());
+        }
+        Console.WriteLine("You got flush!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("No flush!");
+    }
+}
